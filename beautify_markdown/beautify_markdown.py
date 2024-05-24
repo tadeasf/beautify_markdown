73cceb482668110b8b54a393253739f88e2010a3
tadeasf <taddy.fort@gmail.com>
2024-05-24 21:33:30 +0200

    init

---
+++ beautify_markdown/beautify_markdown.py
@@ -0,0 +1,46 @@
+from rich import traceback, Console
+from rich.prompt import Prompt
+import pyperclip
+
+traceback.install()
+
+console = Console()
+
+
+def beautify_markdown(md_to_beautify):
+    md_beautified = md_to_beautify
+    md_beautified = md_beautified.replace(r"\n\n", "\n\n")
+    md_beautified = md_beautified.replace(r"```bash\n", "```bash\n    ")
+    md_beautified = md_beautified.replace(r"```bash\n    ```", "```bash\n```")
+    md_beautified = md_beautified.replace(r"\n- ", "\n**")
+    md_beautified = md_beautified.replace(r"**: ", "**: ")
+    md_beautified = md_beautified.replace(r"\n- **", "\n\n**")
+    return md_beautified
+
+
+def save_to_file(content, filename):
+    with open(filename, "w") as file:
+        file.write(content)
+
+
+def main():
+    filename = Prompt.ask(
+        "Enter the full path to the markdown file you want to output",
+        default="README.md",
+    )
+
+    console.log(
+        "Copy the contents you want to beautify to the clipboard and then press enter"
+    )
+    Prompt.ask("Press enter when you have copied the contents")
+
+    md_to_beautify = pyperclip.paste()
+
+    beautified_md = beautify_markdown(md_to_beautify)
+    save_to_file(beautified_md, filename)
+
+    console.log(f"Beautified markdown has been saved to {filename}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="beautify_markdown",
+    version="1.0.6",
+    author="Tadeas Fort",
+    author_email="taddy.fort@gmail.com",
+    description="A tool to beautify markdown output from chatgpt-cli (https://github.com/marcolardera/chatgpt-cli)",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/tadeasf/beautify_markdown",
+    packages=find_packages(),
+    install_requires=["rich", "pyperclip"],
+    entry_points={
+        "console_scripts": [
+            "beautify_markdown = beautify_markdown.beautify_markdown:main",
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.11",
+)
